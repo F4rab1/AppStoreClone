193bceed82866ff911deccbd4370ddbcd05c65d6
F4rab1 <issafarabi2022@gmail.com>
2024-02-08 10:05:14 +0600

    TabBar

---
+++ AppStoreClone/Controllers/BaseTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  BaseTabBarController.swift
+//  AppStoreClone
+//
+//  Created by Фараби Иса on 08.02.2024.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewControllers = [
+            createNavController(viewController: UIViewController(), title: "Today", imageName: "square"),
+            createNavController(viewController: UIViewController(), title: "Apps", imageName: "square.on.square"),
+            createNavController(viewController: UIViewController(), title: "Search", imageName: "magnifyingglass"),
+        ]
+    }
+    
+    fileprivate func createNavController(viewController: UIViewController, title: String, imageName: String) -> UIViewController {
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.navigationBar.prefersLargeTitles = true
+        viewController.navigationItem.title = title
+        viewController.view.backgroundColor = .white
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(systemName: imageName)
+        return navController
+        
+    }
+    
+}
