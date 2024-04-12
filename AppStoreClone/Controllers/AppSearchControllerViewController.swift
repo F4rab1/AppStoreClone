f860b1fea89a60d4be03f38b8ec48c5924a7f937
F4rab1 <issafarabi2022@gmail.com>
2024-04-12 15:27:01 +0500

    Search Bar implemented

--- AppStoreClone/Controllers/AppSearchControllerViewController.swift
+++ AppStoreClone/Controllers/AppSearchControllerViewController.swift
@@ -6,11 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 import SDWebImage
 
-class AppSearchControllerViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class AppSearchControllerViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     fileprivate let cellId = "id1234"
+    fileprivate let searchController = UISearchController(searchResultsController: nil)
+    
+    fileprivate let enterSearchTermLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Please enter search term above..."
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,13 +29,50 @@ class AppSearchControllerViewController: UICollectionViewController, UICollectio
         collectionView.backgroundColor = .white
         collectionView.register(SearchResultCell.self, forCellWithReuseIdentifier: cellId)
         
+        setupUI()
+        setupConstraints()
+        setupSearchBar()
         fetchITunesApps()
+    }
+    
+    func setupUI() {
+        collectionView.addSubview(enterSearchTermLabel)
+    }
+    
+    func setupConstraints() {
+        enterSearchTermLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    fileprivate func setupSearchBar() {
+        definesPresentationContext = true
+        navigationItem.searchController = self.searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    var timer: Timer?
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        
+        timer?.invalidate()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { (_) in
+            Service.shared.fetchApps(searchTerm: searchText) { (res, err) in
+                self.appResults = res
+                DispatchQueue.main.async {
+                    self.collectionView.reloadData()
+                }
+            }
+        })
     }
     
     fileprivate var appResults = [Result]()
     
     fileprivate func fetchITunesApps() {
-        Service.shared.fetchApps { (results, err) in
+        Service.shared.fetchApps(searchTerm: "instagram") { (results, err) in
             
             if let err = err {
                 print("Failed to fetch apps:", err)
@@ -48,6 +95,7 @@ class AppSearchControllerViewController: UICollectionViewController, UICollectio
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        enterSearchTermLabel.isHidden = appResults.count != 0
         return appResults.count
     }
     
