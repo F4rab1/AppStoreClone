640164e765a6aae33f5151b49eb01aac044f7539
F4rab1 <issafarabi2022@gmail.com>
2024-04-28 23:32:48 +0500

    Passing data into cells

--- AppStoreClone/Controllers/Apps/AppsController.swift
+++ AppStoreClone/Controllers/Apps/AppsController.swift
@@ -11,6 +11,7 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
     
     let cellId = "AppsGroupCellid"
     let headerId = "headerId"
+    var editorsChoiceGames: AppGroup?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +29,10 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
                 return
             }
             
-            print(appGroup?.feed.results)
+            self.editorsChoiceGames = appGroup
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         }
     }
     
@@ -42,11 +46,15 @@ class AppsController: BaseListController, UICollectionViewDelegateFlowLayout {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 1
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! AppsGroupCell
+        cell.titleLabel.text = editorsChoiceGames?.feed.title
+        cell.horizontalController.appGroup = editorsChoiceGames
+        cell.horizontalController.collectionView.reloadData()
+        
         return cell
     }
     
